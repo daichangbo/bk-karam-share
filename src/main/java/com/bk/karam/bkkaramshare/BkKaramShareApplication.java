ef6107b444c8d77cb02d6863eddac35585a8e2f2
daichangbo <2535049537@qq.com>
2019-10-29 20:00:45 +0800

    init

---
+++ src/main/java/com/bk/karam/bkkaramshare/BkKaramShareApplication.java
@@ -0,0 +1,13 @@
+package com.bk.karam.bkkaramshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkKaramShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkKaramShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bk/karam/bkkaramshare/BkKaramShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.karam.bkkaramshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkKaramShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
